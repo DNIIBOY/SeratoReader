8c15fe699ad5b6b43c173f58f177de05a9be0831
DNIIBOY <daniel.nettelfield@gmail.com>
2022-08-17 21:43:10 +0200

    Added better detection, using cv2

--- test.py
+++ test.py
@@ -7,7 +7,15 @@ pytesseract.pytesseract.tesseract_cmd = r"F:\Tesseract\tesseract.exe"
 
 def get_deck_1():
     img = ImageGrab.grab(bbox=(813, 60, 880, 89))
-    return pytesseract.image_to_string(img, config="--psm 6")
+    img.save("test.png")
+
+    img = cv2.imread('test.png', cv2.IMREAD_GRAYSCALE)  
+
+    thresh = cv2.threshold(img, 100, 255, cv2.THRESH_BINARY_INV+cv2.THRESH_OTSU)[1]
+    thresh = cv2.resize(thresh, (0,0), fx=0.9, fy=0.9)  # scale image 0.9X
+
+    detected_text = pytesseract.image_to_string(thresh, config = '--psm 6')
+    return parse(detected_text)
 
 
 def parse(num: str) -> float:
@@ -24,8 +32,9 @@ def parse(num: str) -> float:
 
 if __name__ == "__main__":
     while True:
-        num = get_deck_1()
-        print(num)
-        num = parse(num)
-        print(num)
-        sleep(0.3)
+        print(get_deck_1())
+    #     num = get_deck_1()
+    #     print(num)
+    #     num = parse(num)
+    #     print(num)
+    #     sleep(0.3)
